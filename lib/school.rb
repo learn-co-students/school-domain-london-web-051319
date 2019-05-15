5d8232264fad9e6f043b33e88f33c79fff3ab15d
mbarrins <maija.barrins@gmail.com>
2019-05-15 16:38:57 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+	attr_reader :name, :roster
+
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def add_student(student, grade)
+		@roster[grade] = (@roster[grade] ||= []) << student
+		#@roster.include?(grade) ? @roster[grade] << student : @roster[grade] = [student]
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		@roster.map{|k,v| [k, v.sort]}.to_h
+	end
+end
+
+# school1 = School.new("Bayside High School")
+# school1.add_student("student", 10)
