d6a400d7ae8f77c7a1ba92f9ae8181b4907b39c0
tstiddy <spaceinvadini99@gmail.com>
2019-05-16 07:03:09 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,11 +2,28 @@
 
 class School
 
-  attr_reader(:school)
+  attr_accessor(:name, :classes)
 
-  def intilialize(school)
-    @school = school
+  def intilialize(name)
+    @name = name
+    @classes = {}
   end
 
+  def add_student(student_name, grade)
+    classes[grade] ||= []
+    classes[grade] << student_name
+  end
+
+  def grade(student_grade)
+    classes[student_grade]
+  end
+
+  def sort
+    sorted = {}
+    classes.each do |grade, student|
+      sorted[grade] = student.sort
+    end
+    sorted
+  end
 end
-school = School.new("Test School")
+school = School.new("Bayside High School")
