73f8f7a324689d2256412d54fdb9d10815bfa0e7
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-16 06:27:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, grade_num)
+    roster[grade_num] ||= []
+    roster[grade_num] << student
+  end 
+  
+  def grade(grade_num)
+    roster.detect do |x, y| 
+      if x == grade_num
+        return y 
+      end 
+    end 
+  end 
+  
+  def sort 
+  new_hash = {}
+  roster.each do |x, y| 
+    new_hash[x] = y.sort 
+  end 
+  new_hash
+end 
+  
+end
