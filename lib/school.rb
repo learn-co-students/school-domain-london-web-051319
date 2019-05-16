d421c12d865e9856062db6196436d97a0a395c28
Aaron Summers <aaron.summers2797@gmail.com>
2019-05-16 08:44:34 +0100

    functional code

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+require 'pry'
+# code here!
+class School
+  attr_accessor :roster, :student_name, :grade
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.has_key?(grade) == false
+      @roster[grade] = []
+      #binding.pry
+    end
+      self.roster[grade] << name
+    end
+
+    def grade(grade)
+      @roster[grade]
+    end
+
+    def sort
+      @roster.each do |grade, student_names|
+        @roster[grade] = student_names.sort
+        binding.pry
+      end
+    end
+end
