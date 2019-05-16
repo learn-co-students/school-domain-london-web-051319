8fa16bb14a3d25c8b9073d62cdc4d22a24467ccb
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-05-16 17:41:45 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+
+class School
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, number)
+    if @roster[number] == nil
+      @roster[number]= []
+    end
+    @roster[number] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    newHash = {}
+    @roster.collect do |key, value|
+      newHash[key] = value.sort
+    end
+    newHash
+  end
+end
